e6d50db01e6f610e149cf4ceca26a9148543fdfe
sistechsoftware <uericlislima@gmail.com>
2022-07-08 10:38:58 -0300

    classes de dominio iniciadas

---
+++ src/main/java/br/com/tecnologiasistech/bookstore/domain/Livro.java
@@ -0,0 +1,15 @@
+package br.com.tecnologiasistech.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Livro {
+	
+	private Integer id;
+	private String titulo;
+	private String nome_autor;
+	private String texto;
+	
+	private Categoria categoria;
+
+}
